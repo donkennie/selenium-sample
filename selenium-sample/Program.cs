98ffde4a7db17389c240c2bb1a8d9b3b6030c1ca
donkennie <ajeigbekehinde160@gmail.com>
2024-09-09 07:31:25 +0100

    add test

--- selenium-sample/Program.cs
+++ selenium-sample/Program.cs
@@ -1,28 +1,43 @@
-﻿
+﻿using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using System.Collections.Generic;
+using System.Linq;
 using WebDriverManager;
 using WebDriverManager.DriverConfigs.Impl;
 
-public class Program
+namespace _02Assertions
 {
-    public static void Main(string[] args)
+    public class AssertionsRunner
     {
-        new DriverManager().SetUpDriver(new ChromeConfig());
+        public static void Main(string[] args)
+        {
+            new DriverManager().SetUpDriver(new ChromeConfig());
 
-        IWebDriver driver = new ChromeDriver();
-        driver.Manage().Window.Maximize();
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
 
-        driver.Navigate().GoToUrl("https://github.com/");
+            driver.Navigate().GoToUrl("https://github.com/");
 
-        IWebElement searchBox = driver.FindElement(By.CssSelector(".search-input"));
-        searchBox.Click();
+            string searchPhrase = "selenium";
 
-        IWebElement searchInput = driver.FindElement(By.CssSelector("#query-builder-test"));
-        searchInput.SendKeys("selenium");
-        searchInput.SendKeys(Keys.Enter);
+            IWebElement searchBox = driver.FindElement(By.CssSelector(".search-input"));
+            searchBox.Click();
 
-        driver.Quit();
+            IWebElement searchInput = driver.FindElement(By.CssSelector("#query-builder-test"));
+            searchInput.SendKeys(searchPhrase);
+            searchInput.SendKeys(Keys.Enter);
+
+            IList<string> actualItems = driver.FindElements(By.CssSelector(("[data-testid='results-list'] > div")))
+                .Select(item => item.Text.ToLower())
+                .ToList();
+            IList<string> expectedItems = actualItems
+                .Where(item => item.Contains(searchPhrase))
+                .ToList();
+
+            Assert.That(expectedItems, Is.EqualTo(actualItems));
+
+            driver.Quit();
+        }
     }
-
 }
